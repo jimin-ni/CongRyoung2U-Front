baeb5bda8c535a13e9ad757bc344103c95e17c1b
jimin-ni <jimin326ww@gmail.com>
2023-01-24 11:22:06 +0900

    test 5

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -8,7 +8,10 @@ const GamePage = () => {
     
   return (
     <PageContainer>
-      게임페이지입니다.
+      게임페이지입니다. 
+      <br>
+      아니 잠시만요 이거 어렵네 저 할 수 있겠죠 화이팅입니다....
+      </br>
     </PageContainer>
   );
 };
