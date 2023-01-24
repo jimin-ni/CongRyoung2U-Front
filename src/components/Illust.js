7a290ec44d1b2ff3fc7510ca0bebb10493da6cea
박유정 <p_1321@duksung.ac.kr>
2023-01-24 21:32:26 +0900

    마이페이지 초안

---
+++ src/components/Illust.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import styled from "styled-components";
+
+const IllustContainer = styled.div`
+    height: 357px;
+    width: 357px;
+    background-color: #EAEAEA;
+`
+const IllustBoxContainer = styled.div`
+    margin: 40px;
+    float:left
+`
+
+const Illust = () => {
+    
+    return (
+        <IllustBoxContainer>
+            <IllustContainer>
+                <div>일러스트</div>
+            </IllustContainer>
+            일러스트 이름
+        </IllustBoxContainer>
+    );
+};
+
+export default Illust;
--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,15 +1,46 @@
 import React from 'react';
 import styled from "styled-components";
+import Illust from '../components/Illust';
 
 const PageContainer = styled.div`
-    height: 300px;
+    height: 3000px;
 `
+
+const UserInfoContainer = styled.div`
+  height: 150px;
+  background-color: #252A34;
+  padding: 15px 40px;
+  color: white;
+`
+
+const IconContainer = styled.div`
+  float:right
+`;
+
+
 const MyPage = () => {
     
   return (
     <PageContainer>
+      <UserInfoContainer>
+        <div>
+          <h2>아무개</h2>
+          <h5>아무개 아이디<br/>해금 일러스트 수</h5>
+        </div>
+        <IconContainer>
+          <img className="logo" alt="logo" src="" />
+        </IconContainer>
+      </UserInfoContainer>
       마이페이지입니다.
+      <br/>
+      <Illust/>
+      <Illust/>
+      <Illust/>
+      <Illust/>
+      <Illust/>
+      <Illust/>
     </PageContainer>
+    
   );
 };
 
