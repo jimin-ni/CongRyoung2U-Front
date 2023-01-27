f3e9c7007aa422374136e6fec1f60838e457a205
jimin-ni <jimin326ww@gmail.com>
2023-01-27 20:03:39 +0900

    게임 엔딩 모달창 구조

--- src/pages/GamePage.js
+++ src/pages/GamePage.js
@@ -2,13 +2,12 @@ import React, { useState, useEffect, useRef } from 'react';
 import styled from "styled-components";
 import Navbar from "../components/Navbar";
 import Game from "../components/game/Game";
-//import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { FaMapMarkerAlt } from "react-icons/fa";
 
+//css
 const PageContainer = styled.div`
     height: 500px;
 `
-
-//3개는 좌측 정렬, 하나는 우측정렬
 const MapInfo_Top = styled.div`
   height: 130px;
   background-color: #252A34;
@@ -17,11 +16,9 @@ const MapInfo_Top = styled.div`
   display: flex;
   align-items: center;
 `
-
 const MapInfo_Top_Text = styled.div`
   width: 400px;
 `
-
 const CategoryInfo = styled.div`
   display: inline-block;
   padding: 10px;
@@ -31,32 +28,27 @@ const CategoryInfo = styled.div`
 `
 const PlaceInfo = styled.div`
   display: inline-block;
-  padding: 10px;
+  padding-left: 3px;
   font-size: 20px;
   color: white;
   align-items: center;
 `
-
-// 우측정렬 해야함
 const ItemScoreBox = styled.div` 
   text-decoration: none;
   background-color: #EAEAEA;
-  padding: 15px;
-  margin: 50px;
+  padding: 30px;
+  margin: 10px;
   border-radius: 10px;
   display: flex;
   align-items: center;
   margin-left: auto;
-
 `
 const ItemIMG = styled.div`
 text-decoration: none;
 `
 const ItemScore = styled.div`
 text-decoration: none;
-
 `
-
 const CharacterInfo = styled.div`
   padding: 10px;
   font-size: 35px;
@@ -64,11 +56,9 @@ const CharacterInfo = styled.div`
   align-items: center;
   display: 'flex';
 `
-
 const GameGround = styled.div`
   text-decoration: none;
 `
-
 const MapInfo_Bottom_1 = styled.div`
   height: 200px;
   background-color: #252A34;
@@ -82,7 +72,6 @@ const MapInfo_Bottom_2 = styled.div`
   position: bottom;
   border-radius: 30px 30px 0 0;
   width: 100%;
-
 `
 const IllustItem = styled.div`
   height: 50px;
@@ -103,10 +92,18 @@ const IllustText = styled.div`
   font-size: 20px;
   position:relative;
   margin-top: -20px;
-
+`
+const MapIcon = styled.div`
+color: #FF2E63;
+display: inline-block;
+padding-left: 10px;
+padding-right: 1px;
+align-items: center;
+font-size: 21px;
 `
 
 
+//main code
 const GamePage = () => {
   return (
     <>
@@ -115,6 +112,7 @@ const GamePage = () => {
     <MapInfo_Top> {/* 게임 맵 정보-상단 */}
       <MapInfo_Top_Text>
         <CategoryInfo>카테고리</CategoryInfo>{/*  카테고리 불러오기 */}
+        <MapIcon><FaMapMarkerAlt /></MapIcon>
         <PlaceInfo>장소명</PlaceInfo> {/*  장소명 불러오기 */}
         <CharacterInfo>인물이름</CharacterInfo>
       </MapInfo_Top_Text>
