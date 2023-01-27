d3a0e1130174f344894431809665a958d291f645
박유정 <p_1321@duksung.ac.kr>
2023-01-27 19:45:33 +0900

    마이페이지 api 작성

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import styled from "styled-components";
 import Illust from '../components/Illust';
-import SelectModal from '../components/IllustModal';
+import LockedIllust from '../components/LockedIllust';
+import IllustModal from '../components/IllustModal';
+import LockedIllustModal from '../components/LockedIllustModal';
 import Navbar from "../components/Navbar";
 import axios from "axios";
+import { useSelector } from "react-redux";
 
 const PageContainer = styled.div`
     height: 2800px;
@@ -29,39 +32,65 @@ const IllustTitleContainer = styled.h1`
 
 const MyPage = () => {
   const [modalOpen, setModalOpen] = useState(false);
+  const [illust, setillust] = useState([]); //일러스트 상세 정보
+  const [illustId, setillustId] = useState([]); //선택한 일러스트 (모달창)
+  const [list, setList] = useState([]); //일러스트 전체 목록
+  const [info, setInfo] = useState([]); //유저 정보
+  const [userIllust, setuserIllust] = useState([]); //유저 일러스트 목록
+  const [userItem, setuserItem] = useState([]); //유저 아이템 목록
 
-  const [illust, setillust] = useState([]);
-  
-  const [illustId, setillustId] = useState([]);
+  const userId = useSelector((store) => store.userReducer.userId);
+
   useEffect(() => {
   axios.get(`api/illust/detail/${illustId&&illustId}`).then((response) => {
-      console.log(response.data.illust)
+      //console.log(response.data.illust)
       setillust(response.data.illust)
   })
   },[illustId]);
 
-  const [list, setList] = useState([]);
   useEffect(() => {
-    axios.get("api/illust/list").then((response) => {
-      console.log(response.data.illustList)
-      setList(response.data.illustList)
+    axios.get("api/illust/list").then((response1) => {
+      //console.log(response1.data.illustList)
+      setList(response1.data.illustList)
     })
-  },[])
-  //var list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14];
-  const Illustlist = list?.map((data, index) => {
-    return <Illust data={data} key={index} setModalOpen={setModalOpen} setillustId={setillustId}/>
+
+    let body = {
+      _id: userId&&userId
+    }
+    axios.post("api/users/collection", body).then((response2) => {
+      //console.log(response2.data.collection)
+      let illustList = response2.data.collection.illustList
+      illustList = illustList.map(function(illust){ return illust._id; });
+      setuserIllust(illustList)
+      let itemList = response2.data.collection.itemList
+      itemList = itemList.map(function(item){
+        return {itemName: item.illustId.stageId.itemName, count: item.count};
+      })
+      setuserItem(itemList)
+
+      //console.log(illustList);
+      //console.log(itemList);
+    },[userId])
   });
 
-  const [info, setInfo] = useState([]);
+  const Illustlist = list?.map((data, index) => {
+    return ((userIllust).includes(data._id)) ? (
+      <Illust data={data} key={index} setModalOpen={setModalOpen} setillustId={setillustId}/>
+    ) : (
+      <LockedIllust data={data} key={index} setModalOpen={setModalOpen} setillustId={setillustId}/>
+    )
+  });
+
   useEffect(() => {
     let body = {
-      _id: "63d0217df66263242ab095b0"
+      _id: userId&&userId
     }
     axios.post("api/users/info", body).then((response) => {
-      console.log(response.data.user)
+      //console.log(response.data.user)
       setInfo(response.data.user)
     })
-  },[]);
+  },[userId]);
+
 
   return (
     <PageContainer>
@@ -69,7 +98,7 @@ const MyPage = () => {
       <UserInfoContainer>
         <div style={{float: 'left'}}>
           <h1>{info.name}</h1>
-          <h3>{info.email}<br/>해금 일러스트 수</h3>
+          <h3>{info.email}<br/>해금 일러스트 수 : n / 14</h3>
         </div>
         <IconContainer alt="logo" src={require("../image/main_img_logo.png")}/>
       </UserInfoContainer>
@@ -78,7 +107,7 @@ const MyPage = () => {
         일러스트 목록
       </IllustTitleContainer>
       {Illustlist}
-      {modalOpen && <SelectModal setModalOpen={setModalOpen} data={illust} />}
+      {(modalOpen) && <IllustModal setModalOpen={setModalOpen} data={illust}/>}
     </PageContainer>
     
   );
